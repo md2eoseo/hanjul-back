e2a3ceb856373dd1077d0e59d83386709575b278
md2eoseo <md2eoseo@gmail.com>
2021-03-27 13:38:40 +0900

    prisma, postgresql setup

---
+++ users/seeProfile/seeProfile.resolvers.js
@@ -0,0 +1,17 @@
+import client from "../../client";
+
+export default {
+  Query: {
+    seeProfile: async (_, { username }) => {
+      try {
+        const user = await client.user.findUnique({ where: { username } });
+        if (!user) {
+          return { ok: false, error: "User not found!" };
+        }
+        return { ok: true, user };
+      } catch (error) {
+        return { ok: false, error };
+      }
+    },
+  },
+};
