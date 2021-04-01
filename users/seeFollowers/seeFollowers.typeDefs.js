bdeef79b902087517a05706896169956d7664598
md2eoseo <md2eoseo@gmail.com>
2021-04-01 19:09:07 +0900

    seeFollowing, seeFollowers query

---
+++ users/seeFollowers/seeFollowers.typeDefs.js
@@ -0,0 +1,12 @@
+import { gql } from 'apollo-server-express';
+
+export default gql`
+  type SeeFollowersResult {
+    ok: Boolean!
+    error: String
+    followers: [User]
+  }
+  type Query {
+    seeFollowers(username: String!, lastId: Int): SeeFollowersResult!
+  }
+`;
