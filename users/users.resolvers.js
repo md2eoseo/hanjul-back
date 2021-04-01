7215a09273530b1277d6ab7e0686968423d4b49e
md2eoseo <md2eoseo@gmail.com>
2021-04-01 19:26:01 +0900

    added User custom fields

---
+++ users/users.resolvers.js
@@ -0,0 +1,33 @@
+import client from '../client';
+
+export default {
+  User: {
+    totalFollowers: ({ id }) =>
+      client.user.count({
+        where: { following: { some: { id } } },
+      }),
+    totalFollowing: ({ id }) =>
+      client.user.count({
+        where: { followers: { some: { id } } },
+      }),
+    isMe: ({ id }, _, { loggedInUser }) => (!loggedInUser || id !== loggedInUser.id ? false : true),
+    isFollowing: async ({ id }, _, { loggedInUser }) => {
+      if (!loggedInUser) {
+        return false;
+      }
+      const exists = await client.user.count({
+        where: { id: loggedInUser.id, following: { some: { id } } },
+      });
+      return Boolean(exists);
+    },
+    isFollowers: async ({ id }, _, { loggedInUser }) => {
+      if (!loggedInUser) {
+        return false;
+      }
+      const exists = await client.user.count({
+        where: { id: loggedInUser.id, followers: { some: { id } } },
+      });
+      return Boolean(exists);
+    },
+  },
+};
--- users/users.typeDefs.js
+++ users/users.typeDefs.js
@@ -7,6 +7,15 @@ export default gql`
     lastName: String!
     username: String!
     email: String!
+    bio: String
+    avatar: Upload
+    followers: [User]
+    following: [User]
+    totalFollowers: Int!
+    totalFollowing: Int!
+    isMe: Boolean!
+    isFollowing: Boolean!
+    isFollowers: Boolean!
     createdAt: String!
     updatedAt: String!
   }
