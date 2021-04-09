bf9c188b198177c93be1aa0155f89efbaf6c6f49
md2eoseo <md2eoseo@gmail.com>
2021-04-09 12:47:48 +0900

    only ADMIN can CUD the words

--- words/createWord/createWord.resolvers.js
+++ words/createWord/createWord.resolvers.js
@@ -1,9 +1,13 @@
+import { Role } from '@prisma/client';
 import client from '../../client';
 
 export default {
   Mutation: {
-    createWord: async (_, { word, meaning, root, variation, date }) => {
+    createWord: async (_, { word, meaning, root, variation, date }, { loggedInUser }) => {
       try {
+        if (loggedInUser.role !== Role.ADMIN) {
+          return { ok: false, error: 'Only administrator can create words.' };
+        }
         await client.word.create({
           data: { word, meaning, root, variation, date },
         });
--- words/deleteWord/deleteWord.resolvers.js
+++ words/deleteWord/deleteWord.resolvers.js
@@ -1,9 +1,13 @@
+import { Role } from '@prisma/client';
 import client from '../../client';
 
 export default {
   Mutation: {
-    deleteWord: async (_, { id }) => {
+    deleteWord: async (_, { id }, { loggedInUser }) => {
       try {
+        if (loggedInUser.role !== Role.ADMIN) {
+          return { ok: false, error: 'Only administrator can delete words.' };
+        }
         const word = await client.word.findUnique({ where: { id } });
         if (!word) {
           return { ok: false, error: 'Word not found!' };
