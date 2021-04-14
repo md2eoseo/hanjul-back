01e942aeaba2d524957e7d2aac92233d79e741ff
md2eoseo <md2eoseo@gmail.com>
2021-04-15 00:29:46 +0900

    seeDayFeed query 에러 핸들링

--- src/posts/seeDayFeed/seeDayFeed.resolvers.js
+++ src/posts/seeDayFeed/seeDayFeed.resolvers.js
@@ -4,12 +4,15 @@ const resolverFn = async (_, { lastId }) => {
   try {
     const PAGE_SIZE = 20;
     const dateTime = new Date();
-    console.log(typeof dateTime.getFullYear());
     const formatDateTime =
       dateTime.getFullYear().toString().slice(2) +
       (dateTime.getMonth() + 1).toString().padStart(2, '0') +
       dateTime.getDate().toString().padStart(2, '0');
-    const { id: wordId } = await client.word.findFirst({ where: { date: formatDateTime } });
+    const word = await client.word.findFirst({ where: { date: formatDateTime } });
+    if (!word) {
+      return { ok: false, error: 'There is no word for today.' };
+    }
+    const wordId = word.id;
     const posts = await client.post.findMany({
       where: { wordId },
       include: { author: true },
