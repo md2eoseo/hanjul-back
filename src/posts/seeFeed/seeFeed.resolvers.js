1aaaa84af7a442e19529d0e386e44bcccdd50418
md2eoseo <md2eoseo@gmail.com>
2021-05-04 14:27:08 +0900

    팔로잉 작가 글, 좋아요 한 글 조회하도록 seeFeed Query 수정

--- src/posts/seeFeed/seeFeed.resolvers.js
+++ src/posts/seeFeed/seeFeed.resolvers.js
@@ -5,7 +5,7 @@ const resolverFn = async (_, { lastId }, { loggedInUser }) => {
   try {
     const PAGE_SIZE = 20;
     const posts = await client.post.findMany({
-      where: { author: { followers: { some: { id: loggedInUser.id } } } },
+      where: { OR: [{ author: { followers: { some: { id: loggedInUser.id } } } }, { likes: { some: { userId: loggedInUser.id } } }] },
       include: { author: true },
       orderBy: { createdAt: 'desc' },
       take: PAGE_SIZE,
