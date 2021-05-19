748d12d32ff44ce33f9bc484b304b0927f1d6c8b
md2eoseo <md2eoseo@gmail.com>
2021-05-19 15:59:44 +0900

    removed select option for searchWords Query

--- src/words/searchWords/searchWords.resolvers.js
+++ src/words/searchWords/searchWords.resolvers.js
@@ -7,7 +7,6 @@ export default {
         const PAGE_SIZE = 8;
         const words = await client.word.findMany({
           where: { word: { contains: word }, date },
-          select: { id: true, word: true, date: true },
           skip: lastId ? 1 : 0,
           take: PAGE_SIZE,
           ...(lastId && { cursor: { id: lastId } }),
