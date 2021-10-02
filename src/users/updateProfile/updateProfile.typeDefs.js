5588f120082bc8bcaf5d92a81847983cb9e3e463
md2eoseo <md2eoseo@gmail.com>
2021-10-02 16:23:10 +0900

    fix: 계정 정보 변경 Mutation에서 비밀번호 변경 제거

--- src/users/updateProfile/updateProfile.typeDefs.js
+++ src/users/updateProfile/updateProfile.typeDefs.js
@@ -2,14 +2,6 @@ import { gql } from 'apollo-server-express';
 
 export default gql`
   type Mutation {
-    updateProfile(
-      firstName: String
-      lastName: String
-      username: String
-      email: String
-      password: String
-      bio: String
-      avatar: Upload
-    ): BaseResponse!
+    updateProfile(firstName: String, lastName: String, username: String, bio: String, avatar: Upload): BaseResponse!
   }
 `;
